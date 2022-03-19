dc549082bc2151c27df44d3daa7eca29f8fae299
Thuta <thutaminthway2001@gmail.com>
2022-03-19 20:27:16 +0630

    compiling typescript

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var text = "Go Away JS.";
+console.log(text);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,7 @@
+const text = "Go Away JS.";
+console.log(text);
+
+const inputs = document.querySelectorAll('input');
+inputs.forEach((input) => {
+    console.log(input);
+})
