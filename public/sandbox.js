bdda23c88ae7eff76d70a9688f4480f36c40e367
Thuta <thutaminthway2001@gmail.com>
2022-03-20 15:45:40 +0630

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("I love Typescript.");
+let sayName;
+sayName = () => {
+    console.log('Hello Typescript.');
+};
+sayName();
+const add = (a, b, c = 3) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(1, 2, "hello");
+const subtract = (a, b) => {
+    return a - b;
+};
+let minus = subtract(10, 5); //5
+console.log(minus);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,18 @@
-console.log("I love Typescript.");
+let sayName: Function;
+
+sayName = () => {
+    console.log('Hello Typescript.');
+}
+sayName();
+
+const add = (a:number, b:number, c: number | string = 3):void => {
+    console.log(a + b);
+    console.log(c);
+}
+add(1, 2, "hello");
+
+const subtract = (a:number, b:number):number => {
+   return a - b;
+}
+let minus = subtract(10, 5); //5
+console.log(minus);
